9889e1db91cd4c4232a3b50052b2927469c502c2
Ryoma Kai <legknmj@icloud.com>
2023-01-16 21:42:04 +0900

    refactoring and bugfix

--- README.md
+++ README.md
@@ -1,24 +1,26 @@
+# homebridge-natureremo-fan
 
-homubridge用 NatureRemo Cloud API特化型FAN制御プラグイン （NatureRemo Cloud API specialized FAN control plugin）
+- homebridge NatureRemo Cloud API specialized FAN control plugin
+- forked from [mizuka-ninomae/homebridge-nature-remo-cloud-fan](https://github.com/mizuka-ninomae/homebridge-nature-remo-cloud-fan)
 
-Homebridge：  https：//github.com/nfarina/homebridge
+## function
 
-ＯＦＦ（0%） ⇔ ＬＯＷ（33%） ⇔ ＭＩＤＤＬＥ（66%） ⇔ ＨＩＧＨ（100%）
+- speed
+  - OFF(0%)
+  - LOW(33%)
+  - MIDDLE(66%)
+  - HIGH(100%)
+- direction
+  - CLOCKWISE
+  - COUNTERCLOCKWISE
 
-時計回り（CLOCKWISE） ⇔ 反時計回り（CLOCKWISE）
+## config
 
-の設定が可能です。
-
-汎用型バージョン
-https://www.npmjs.com/package/homebridge-cmd-fan
-
-config.json 記入例
-```js
-
+```json
   "accessories": [
       {
           "accessory": "NatureRemoFan",
-          "name": "シーリングファン",
+          "name": "Fan",
           "access_token": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
           "high_signal_ID": "XXXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX",
           "middle_signal_ID": "XXXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX",
@@ -27,27 +29,5 @@ config.json 記入例
           "clockwise_signal_ID": "XXXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX",
           "c_clockwise_signal_ID": "XXXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"
       },
-      {
-        ～
-      }
   ]
 ```
-
-* `accessory` → 固定値（Fixed value）
-* `name` → お好みに（To your liking）
-* `access_token` → 公式HPより取得（Obtained from the official website）
-* `~signal_ID` → 公式HPより取得（Obtained from the official website）それぞれの場合のsignal IDを記入してください。（Enter the signal ID for each case）
-* `language` → en（ENGLISH:[Default]）、jp（JAPANESE:ログの高中低、時計回り、反時計回りの表示が日本語になってメモが付く）
-
-公式HP（official website）https://developer.nature.global/
-access token発行ページ（access token issuing site）https://home.nature.global/
-
-取得方法は「nature remo」「access token」「signal ID」「発行」等でGoogleで検索をかければ、手順がいくらでも出てくるので省略。
-
-テスト環境（testing environment）
-
-・Raspberry Pi3
-
-・Nature Remo 1ST generation
-
-・どれが商品名か分からないこの商品 https://item.rakuten.co.jp/low-ya/fc03-g1003-100/?s-id=ph_pc_itemname
--- index.js
+++ index.js
@@ -1,199 +1,155 @@
-let Service, Characteristic;
-let exec         = require("child_process").exec;
+let Service, Characteristic
+const exec = require('child_process').exec
 
-module.exports = function(homebridge){
-  Service        = homebridge.hap.Service;
-  Characteristic = homebridge.hap.Characteristic;
-  homebridge.registerAccessory("homebridge-nature-remo-cloud-fan", "NatureRemoFan", FanAccessory);
+module.exports = function (homebridge) {
+  Service = homebridge.hap.Service
+  Characteristic = homebridge.hap.Characteristic
+  homebridge.registerAccessory('homebridge-natureremo-fan', 'NatureRemoFan', FanAccessory)
 }
 
-function FanAccessory(log, config) {
-  this.log                   = log;
-  this.name                  = config["name"];
-  this.access_token          = config["access_token"];
-  this.high_signal_ID        = config["high_signal_ID"];
-  this.middle_signal_ID      = config["middle_signal_ID"];
-  this.low_signal_ID         = config["low_signal_ID"];
-  this.off_signal_ID         = config["off_signal_ID"];
-  this.Use_Counter_Clockwise = config["Use_Counter_Clockwise"] || false;
-　this.clockwise_signal_ID   = config["clockwise_signal_ID"];
-  this.c_clockwise_signal_ID = config["c_clockwise_signal_ID"];
-  this.language              = config["language"] || "en";
+function FanAccessory (log, config) {
+  this.log = log
+  this.name = config.name
+  this.access_token = config.access_token
+  this.high_signal_ID = config.high_signal_ID
+  this.middle_signal_ID = config.middle_signal_ID
+  this.low_signal_ID = config.low_signal_ID
+  this.off_signal_ID = config.off_signal_ID
+  this.Use_Counter_Clockwise = config.Use_Counter_Clockwise || false
+  this.clockwise_signal_ID = config.clockwise_signal_ID
+  this.c_clockwise_signal_ID = config.c_clockwise_signal_ID
   this.state = {
     power: false,
-    speed: 0,
-  };
+    speed: 0
+  }
 
-  this.informationService    = new Service.AccessoryInformation();
-  this.fanService            = new Service.Fan(this.name);
+  this.informationService = new Service.AccessoryInformation()
+  this.fanService = new Service.Fan(this.name)
 
   this.informationService
-  .setCharacteristic(Characteristic.Manufacturer, "NatureRemo-FAN Manufacturer")
-  .setCharacteristic(Characteristic.Model, "NatureRemo-FAN Model")
-  .setCharacteristic(Characteristic.SerialNumber, "NatureRemo-FAN Serial Number");
+    .setCharacteristic(Characteristic.Manufacturer, 'NatureRemo-FAN Manufacturer')
+    .setCharacteristic(Characteristic.Model, 'NatureRemo-FAN Model')
+    .setCharacteristic(Characteristic.SerialNumber, 'NatureRemo-FAN Serial Number')
 
   this.fanService
-  .getCharacteristic(Characteristic.On)
-  .on('set', this.setOn.bind(this))
+    .getCharacteristic(Characteristic.On)
+    .on('set', this.setOn.bind(this))
 
   this.fanService
-  .getCharacteristic(Characteristic.RotationSpeed)
-  .setProps({
-     minValue: 0,
-     maxValue: 99,
-     minStep:  33,
-  })
-  .on('set', this.setSpeed.bind(this))
+    .getCharacteristic(Characteristic.RotationSpeed)
+    .setProps({
+      minValue: 0,
+      maxValue: 99,
+      minStep: 33
+    })
+    .on('set', this.setSpeed.bind(this))
 
   this.fanService
-  .getCharacteristic(Characteristic.RotationDirection)
-  .on('set', this.setDirection.bind(this));
+    .getCharacteristic(Characteristic.RotationDirection)
+    .on('set', this.setDirection.bind(this))
 }
 
-//------------------------------------------------------------------------------
-FanAccessory.prototype.getServices = function() {
-  return [this.informationService, this.fanService];
+// ------------------------------------------------------------------------------
+FanAccessory.prototype.getServices = function () {
+  return [this.informationService, this.fanService]
 }
 
-//------------------------------------------------------------------------------
-FanAccessory.prototype.setOn = function(value, callback) {
-  if (this.state.power != value) {
-    if      (this.language == 'en') {
-      this.log(' <<<< [Power Button: ' + value + ']');
-    }
-    else if (this.language == 'jp') {
-      this.log(' <<<< [電源ボタン: ' + value + ']');
-    }
-    this.state.power = value;
-    this.setFanState(this.state, callback);
-  }
-  else {
-    callback(null);
+// ------------------------------------------------------------------------------
+FanAccessory.prototype.setOn = function (value, callback) {
+  if (this.state.power !== value) {
+    this.log(' <<<< [Power Button: ' + value + ']')
+    this.state.power = value
+    this.setFanState(this.state, callback)
+  } else {
+    callback(null)
   }
 }
-//------------------------------------------------------------------------------
-FanAccessory.prototype.setSpeed = function(value, callback) {
-  if (this.state.speed != value) {
-    if (value == 0) {
-      this.state.power = false;
+// ------------------------------------------------------------------------------
+FanAccessory.prototype.setSpeed = function (value, callback) {
+  if (this.state.speed !== value) {
+    if (value === 0) {
+      this.state.power = false
+    } else {
+      this.state.power = true
     }
-    else {
-      this.state.power = true;
-    }
-    this.state.speed = value;
-    this.setFanState(this.state, callback);
-  }
-  else {
-    callback(null);
+    this.state.speed = value
+    this.setFanState(this.state, callback)
+  } else {
+    callback(null)
   }
 }
 
-//------------------------------------------------------------------------------
-FanAccessory.prototype.setDirection = function(value, callback) {
-  if (this.state.direction != value) {
-    this.state.direction = value;
-    this.setFanState2(this.state, callback);
-  }
-  else {
-    callback(null);
+// ------------------------------------------------------------------------------
+FanAccessory.prototype.setDirection = function (value, callback) {
+  if (this.state.direction !== value) {
+    this.state.direction = value
+    this.setFanState2(this.state, callback)
+  } else {
+    callback(null)
   }
 }
-//------------------------------------------------------------------------------
-FanAccessory.prototype.setFanState = function(state, callback) {
-  let signal_ID;
+// ------------------------------------------------------------------------------
+FanAccessory.prototype.setFanState = function (state, callback) {
+  let signalId
+  this.log('[Power: ' + state.power + ']')
   if (state.power) {
-    if      (state.speed == 33) {
-      signal_ID = this.middle_signal_ID;
-      if      (this.language == 'en') {
-        this.log(' <<<< [Power: ' + state.power + ']  [FANSpeed: LOW(' + state.speed + '%)]');
-      }
-      else if (this.language == 'jp') {
-        this.log(' <<<< [電源: ' + state.power + ']  [スピード: 低(' + state.speed + '％)]');
-      }
+    this.log('[FANSpeed: ' + state.speed + '%]')
+    switch (state.speed) {
+      case 33:
+        signalId = this.low_signal_ID
+        break
+      case 66:
+        signalId = this.middle_signal_ID
+        break
+      case 99:
+        signalId = this.high_signal_ID
+        break
     }
-    else if (state.speed == 66) {
-      signal_ID = this.middle_signal_ID;
-      if      (this.language == 'en') {
-        this.log(' <<<< [Power: ' + state.power + ']  [FANSpeed: MIDDLE(' + state.speed + '%)]');
-      }
-      else if (this.language == 'jp') {
-        this.log(' <<<< [電源: ' + state.power + ']  [スピード: 中(' + state.speed + '％)]');
-      }
-    }
-    else if (state.speed == 99) {
-      signal_ID = this.high_signal_ID;
-      if      (this.language == 'en') {
-        this.log(' <<<< [Power: ' + state.power + ']  [FANSpeed: HIGH(' + state.speed + '%)]');
-      }
-      else if (this.language == 'jp') {
-        this.log(' <<<< [電源: ' + state.power + ']  [スピード: 高(' + state.speed + '％)]');
-      }
-    }
-  }
-  else {
-      signal_ID = this.off_signal_ID;
-      if      (this.language == 'en') {
-        this.log('[Power: ' + state.power + ']');
-      }
-      else if (this.language == 'jp') {
-        this.log('[電源: ' + state.power + ']');
-    }
+  } else {
+    signalId = this.off_signal_ID
   }
 
-  this.cmdRequest(signal_ID, function(error, stdout, stderr) {
+  this.cmdRequest(signalId, function (error, stdout, stderr) {
     if (error) {
-      this.log('Function Failed', error);
-      callback(error);
+      this.log('Function Failed', error)
+      callback(error)
+    } else {
+      callback()
     }
-    else {
-      callback();
-    }
-  }.bind(this));
+  }.bind(this))
 }
 
-//------------------------------------------------------------------------------
-FanAccessory.prototype.setFanState2 = function(state, callback) {
-  let signal_ID;
-  if (state.direction == 0) {
-    signal_ID = this.clockwise_signal_ID;
-    if      (this.language == 'en') {
-      this.log(' <<<< [Direction: CLOCKWISE]');
-    }
-    else if (this.language == 'jp') {
-      this.log(' <<<< [回転方向: 時計回り（冬用上向き）]');
-    }
-  }
-  else {
-    signal_ID = this.c_clockwise_signal_ID;
-    if      (this.language == 'en') {
-      this.log(' <<<< [Direction: COUNTER CLOCKWISE]');
-    }
-    else if (this.language == 'jp') {
-      this.log(' <<<< [回転方向: 反時計回り（夏用下向き）]');
-    }
+// ------------------------------------------------------------------------------
+FanAccessory.prototype.setFanState2 = function (state, callback) {
+  let signalId
+  if (state.direction === 0) {
+    signalId = this.clockwise_signal_ID
+    this.log('[Direction: CLOCKWISE]')
+  } else {
+    this.log('[Direction: COUNTER CLOCKWISE]')
+    signalId = this.c_clockwise_signal_ID
   }
 
-  this.cmdRequest(signal_ID, function(error, stdout, stderr) {
+  this.cmdRequest(signalId, function (error, stdout, stderr) {
     if (error) {
-      this.log('Function Failed', error);
-      callback(error);
+      this.log('Function Failed', error)
+      callback(error)
+    } else {
+      callback()
     }
-    else {
-      callback();
-    }
-  }.bind(this));
+  }.bind(this))
 }
 
-//------------------------------------------------------------------------------
-FanAccessory.prototype.cmdRequest = function(signal_ID, callback) {
-  let url       = ' "https://api.nature.global/1/signals/' + signal_ID + '/send"';
-  let param_1   = 'curl -X POST';
-  let param_2   = ' -H "accept":"application/json"';
-  let param_3   = ' -k --header "Authorization":"Bearer';
-  let param_4   = ' ' + this.access_token + '"';
-  let param_all = param_1 + url + param_2 + param_3 + param_4;
+// ------------------------------------------------------------------------------
+FanAccessory.prototype.cmdRequest = function (signalId, callback) {
+  const url = ' "https://api.nature.global/1/signals/' + signalId + '/send"'
+  const param1 = 'curl -X POST'
+  const param2 = ' -H "accept":"application/json"'
+  const param3 = ' -k --header "Authorization":"Bearer'
+  const param4 = ' ' + this.access_token + '"'
+  const paramAll = param1 + url + param2 + param3 + param4
 
-  exec(param_all, function(error, stdout, stderr) {
-	  callback(error, stdout, stderr)
+  exec(paramAll, function (error, stdout, stderr) {
+    callback(error, stdout, stderr)
   })
 }
